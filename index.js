3191b8c615b538c29b7c4c9ecebb758d700b3dcc
Arnaud Casamé <djcase001@gmail.com>
2017-12-29 19:59:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const app = "I don't do much.";
 let kittens = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendKitten(arr, item){
-  arr.push(item);
-  return arr;
+function destructivelyAppendKitten(kitten){
+  kittens.push(kitten);
+  return kittens;
 }
 
 function appendKitten(arr, item){
